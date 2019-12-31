f544acb4920d737296b1c221bb8c4a51c92f4d9c
Think Wu <yingxinwu.g@gmail.com>
2019-12-31 16:36:26 +0800

    update model

--- digits_mnist.py
+++ digits_mnist.py
@@ -10,7 +10,13 @@ print("mnist dataset: train=%s test=%s" % (x_train.shape, x_test.shape))
 x_train, x_test = x_train / 255.0, x_test / 255.0  # normalizing
 model = keras.models.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(512, activation='relu'),
+    keras.layers.Dropout(0.2),
+    keras.layers.Dense(512, activation='relu'),
+    keras.layers.Dropout(0.2),
+    keras.layers.Dense(256, activation='relu'),
+    keras.layers.Dropout(0.2),
+    keras.layers.Dense(256, activation='relu'),
     keras.layers.Dropout(0.2),
     keras.layers.Dense(10, activation='softmax'),
 ])
@@ -21,14 +27,18 @@ model.compile(optimizer='adam',
 
 class myCallback(keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs={}):
-        self.model.stop_training = logs.get('accuracy') > 0.99
+        self.model.stop_training = logs.get('accuracy') > 0.993
 
 
+# model.fit(x_train,
+#           y_train,
+#           epochs=50)
 model.fit(x_train,
           y_train,
           epochs=50,
           callbacks=[myCallback()])
 model.evaluate(x_test, y_test)
+
 tf.saved_model.save(model, 'models/digits_mnist')
 converter = tf.lite.TFLiteConverter.from_saved_model('models/digits_mnist')
 lite_model = converter.convert()
